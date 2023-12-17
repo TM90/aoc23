3a6c73045228980839ac61e498b7fd439825ce6e
TM90 <tobias_markus@gmx.net>
2023-12-17 21:29:54 +0100

    initial solution for day 4 part 1 freeing memory must still be done

--- 4/src/main.zig
+++ 4/src/main.zig
@@ -31,19 +31,43 @@ const Card = struct {
             .numbers_you_have = numbers_you_have,
         };
     }
+
+    fn matches(self: Card) u64 {
+        var sum: u64 = 0;
+        for (self.numbers_you_have.items) |number| {
+            for (self.winning_numbers.items) |winner| {
+                if (number == winner) {
+                    sum += 1;
+                }
+            }
+        }
+        return sum;
+    }
+    pub fn points(self: Card) u64 {
+        const n = self.matches();
+        var sum: u64 = 0;
+        for (0..n) |value| {
+            if (value >= 1) {
+                sum += std.math.powi(u64, 2, value - 1) catch unreachable;
+            } else {
+                sum += 1;
+            }
+        }
+        return sum;
+    }
 };
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    const path = "input_simple.dat";
+    const path = "input.dat";
     const file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
     var eof = false;
     var buffer: [1024]u8 = undefined;
     var fbs = std.io.fixedBufferStream(&buffer);
-
+    var sum_1a: u64 = 0;
     while (!eof) {
         var winning_number_allocator = std.heap.GeneralPurposeAllocator(.{}){};
         var current_numbers_allocator = std.heap.GeneralPurposeAllocator(.{}){};
@@ -53,11 +77,10 @@ pub fn main() !void {
         };
         const line = fbs.getWritten();
         const card = Card.parse(winning_number_allocator.allocator(), current_numbers_allocator.allocator(), line);
-        _ = card;
-
+        sum_1a += card.points();
         fbs.reset();
     }
-    try stdout.print("Result 1a:\n", .{});
+    try stdout.print("Result 1a: {d}\n", .{sum_1a});
     try stdout.print("Result 1b:\n", .{});
     try bw.flush();
 }
